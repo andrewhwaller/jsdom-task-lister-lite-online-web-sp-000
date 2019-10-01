2d0a4862055a7f1e17a7bee3b69190d35029c81f
Andrew Waller <andrewhwaller@gmail.com>
2019-10-01 01:02:27 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
-
+  document.querySelector('form').addEventListener('submit', onSubmit)
 });
+
+function onSubmit(event) {
+  event.preventDefault()
+  const newTask = document.querySelector('input').value
+
+}
